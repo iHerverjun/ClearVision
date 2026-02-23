5aad9f81b4cf7665768ff75de0e9772b2d09fa1d
HerverJun <yzhipeng22@gmail.com>
2026-02-23 18:43:52 +0800

    决定暂时弃置

---
+++ Acme.Product/src/Acme.Product.Desktop/wwwroot/assets/trash-CX_H5DO_.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-DfMseTtP.js";const t=a("chevron-up",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);const h=a("trash",[["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]]);export{t as C,h as T};
